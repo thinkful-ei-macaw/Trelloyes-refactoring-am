ddd9910ca2b0346bf96d1c8fc9f8eafc3f6b7b71
Agatha North <rouborn@gmail.com>
2020-03-05 09:23:09 -0800

    Updates

---
+++ precache-manifest.907706c228dcc0d468369c392faed1ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccfb17458295556ce2f8",
+    "url": "/Trelloyes-refactoring-am/static/css/main.2d4bdbc9.chunk.css"
+  },
+  {
+    "revision": "ccfb17458295556ce2f8",
+    "url": "/Trelloyes-refactoring-am/static/js/main.c4d3bd0c.chunk.js"
+  },
+  {
+    "revision": "db50082227cf9cfc1e6d",
+    "url": "/Trelloyes-refactoring-am/static/js/runtime~main.81681b80.js"
+  },
+  {
+    "revision": "b629ef0911bfcc787878",
+    "url": "/Trelloyes-refactoring-am/static/js/2.8eadc589.chunk.js"
+  },
+  {
+    "revision": "bc85982315f6271d29bbb9fdf35ecf69",
+    "url": "/Trelloyes-refactoring-am/index.html"
+  }
+];
